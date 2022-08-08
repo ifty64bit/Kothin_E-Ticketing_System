c11616275b3741220fe3e6fe36c73a6286cec104
ifty64bit <19-40447-1@student.aiub.edu>
2022-08-08 23:30:50 +0600

    gg

--- Business_Logic/BOs/TrainModel.cs
+++ Business_Logic/BOs/TrainModel.cs
@@ -9,7 +9,6 @@ namespace BusinessLogic.BOs
     public class TrainModel
     {
         public int Id { get; set; }
-        public int Id2 { get; set; }
         public string Number { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
